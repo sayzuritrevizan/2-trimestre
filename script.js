8d3382d0ff2bb88654d14421ac9b19ac8e3d3e9a
sayzuritrevizan <148063702+sayzuritrevizan@users.noreply.github.com>
2024-07-31 10:13:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Qual sua opinião sobre o chat gpt",
-            alternativa:[
+            alternativa: [
     {
             texto: "Isso é assustador!",
             afirmacao:"porque e uma tecnologia muito avancada"
@@ -25,28 +25,33 @@ const perguntas = [
                 texto: "foi um avanco, nao perdemos tempo buscando respostas",
             afirmacao:"em parte,sim, ajudou o gasto de tmepo buscando respostas"
                 },  
-            {
-       ]
+            ]
     },
     {
         enunciado: "Após a elaboração do trabalho, o professor realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
         alternativas: [
+            {
             "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
             "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores."
+            },
         ]
     },
     {
         enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
         alternativas: [
+            {
             "Criar uma imagem utilizando uma plataforma de design como o Paint.",
             "Criar uma imagem utilizando um gerador de imagem de IA."
+            },
         ]
     },
     {
         enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda de uma IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz?",
         alternativas: [
+            {
            "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
             "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial."
+            },
         ]
     },
 ];
